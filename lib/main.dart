7b2f733ff053a777d21e5e0125719132a98312e6
Sameh <samehmaher.business@gmail.com>
2021-01-05 23:36:30 +0200

    photo stop

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,52 @@
 import 'package:flutter/material.dart';
+
+main() {
+  runApp(Home());
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Subsciptions',
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  CircleAvatar(
+                    radius: 20,
+                    backgroundColor: Colors.orange,
+                    child: Icon(
+                      Icons.add,
+                      size: 20,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Card(
+              child: Row(
+                children: [
+                  // stop in photo
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
